95cb383fef55ded8fe1b995e711cd8dfa93ea4e8
Favour Chidera Emeka <favorodera@gmail.com>
2024-04-25 15:47:56 +0100

    added theme change features and corrected some class issues

--- assets/js/webpage.jsx
+++ assets/js/webpage.jsx
@@ -1,9 +1,84 @@
-import React from "react";
+import React, { useState } from "react";
 import "/assets/css/webpage.css";
+import darkGithub from "/assets/media/icons/darkThemeGithubIcon.svg"
+import sun from "/assets/media/icons/sun.svg"
+import moon from "/assets/media/icons/moon.svg"
+import lightGithub from "/assets/media/icons/lightThemeGithubIcon.svg"
 
 function Webpage() {
 
-    const inputFontSizeHandler = (event) => {
+    let [darkMode, setMode] = useState(true)
+
+    const setTheme = (event) => {
+        let inputs = document.querySelectorAll("input")
+        let pauseButton = document.querySelector(".pauseButton")
+        let webpageContainer = document.querySelector(".webpageContainer")
+        let body = document.body
+        let githubIconContainer = document.querySelector(".githubIconContainer")
+        let githubIcon = document.querySelector(".githubIcon")
+        let themeChangerIconContainer = document.querySelector(".themeChangerIconContainer")
+        let themeChangerIcon = document.querySelector(".themeChangerIcon")
+        let timerContainer = document.querySelector(".timerContainer")
+        let timerHeader = document.querySelector(".timerHeader")
+        let labels = document.querySelectorAll("label")
+
+
+        event.preventDefault()
+
+        if (darkMode === true) {
+
+            setMode(darkMode = false)
+
+            body.style.backgroundColor = "rgba(255, 255, 255, 1)"
+            webpageContainer.style.backgroundColor = "rgba(255, 255, 255, 1)"
+            themeChangerIcon.src = moon
+            themeChangerIconContainer.style.cssText = "background-color:rgba(233, 233, 233, 1)"
+            githubIcon.src = lightGithub
+            githubIconContainer.style.cssText = "background-color:rgba(233, 233, 233, 1)"
+            timerContainer.style.cssText = "background-color: rgba(249, 249, 249, 1);"
+            timerHeader.style.cssText = "color:rgba(0, 0, 0, 1)"
+
+
+            for (const input of inputs) {
+
+                input.setAttribute("class", "lightModeInput")
+            }
+
+            for (const label of labels) {
+                label.style.cssText = "color:rgba(0, 0, 0, 1);"
+            }
+
+            pauseButton.style.cssText = "color:rgba(50, 50, 50, 1); background-color:rgba(249, 249, 249, 1); border: 1px solid rgba(209, 209, 209, 1)"
+
+
+        } else {
+
+            setMode(darkMode = true)
+
+            body.style.backgroundColor = "rgba(1, 19, 30, 1)"
+            webpageContainer.style.backgroundColor = "rgba(1, 19, 30, 1)"
+            themeChangerIcon.src = sun
+            themeChangerIconContainer.style.cssText = "background-color: rgba(0, 39, 63, 1)"
+            githubIcon.src = darkGithub
+            githubIconContainer.style.cssText = "background-color: rgba(0, 39, 63, 1)"
+            timerContainer.style.cssText = "background-color: rgba(1, 28, 44, 1);"
+            timerHeader.style.cssText = "color: rgba(255, 255, 255, 1)"
+
+            for (const input of inputs) {
+
+                input.setAttribute("class", "darkModeInput")
+            }
+
+            for (const label of labels) {
+                label.style.cssText = "rgba(255, 255, 255, 1);"
+            }
+
+            pauseButton.style.cssText = "rgba(255, 255, 255, 1); background-color:rgba(234, 234, 234, 0); border: 1px solid rgba(209, 209, 209, 1)"
+
+        }
+    }
+
+    const inputFontSizeHandler = () => {
 
         let inputs = document.querySelectorAll("input")
 
@@ -12,22 +87,38 @@ function Webpage() {
             if (input.value.length === 3) {
 
                 input.style.cssText = "font-size: 30px;"
-                input.setAttribute("class", "threeValueInput")
+
+                if (darkMode === false) {
+                    input.classList.toggle("lightModeThreeValueInput")
+                } else {
+                    input.classList.toggle("darkModeThreeValueInput")
+                }
+
 
             } else if (input.value.length === 4) {
 
                 input.style.cssText = "font-size: 20px;"
-                input.setAttribute("class", "fourValueInput")
+
+                if (darkMode === false) {
+                    input.classList.toggle("lightModeFourValueInput")
+                } else {
+                    input.classList.toggle("darkModeFourValueInput")
+                }
 
             } else if (input.value.length >= 5) {
 
                 input.style.cssText = "font-size: 15px;"
-                input.setAttribute("class", "fiveOrMoreThanFiveValueInput")
+
+                if (darkMode === false) {
+                    input.classList.toggle("darkModeFiveOrMoreThanFiveValueInput")
+                } else {
+                    input.classList.toggle("darkModeFiveOrMoreThanFiveValueInput")
+                }
 
             } else if (input.value.length < 3) {
 
                 input.style.cssText = "font-size: 45px;"
-                input.removeAttribute("class")
+                input.classList.remove("darkModeFiveOrMoreThanFiveValueInput")
 
             }
 
@@ -44,6 +135,7 @@ function Webpage() {
         let pauseButton = document.querySelector(".pauseButton")
         let resetButton = document.querySelector(".resetButton")
         let errorMessage = document.querySelector(".errorMessage");
+
 
         for (const input of inputs) {
 
@@ -92,7 +184,12 @@ function Webpage() {
                 for (const input of inputs) {
 
                     input.style.cssText = "font-size: 30px;"
-                    input.setAttribute("class", "threeValueInput")
+
+                    if (darkMode === false) {
+                        input.classList.add("lightModeThreeValueInput")
+                    } else {
+                        input.classList.add("darkModeThreeValueInput")
+                    }
 
                 }
             } else if (secondsInput.value.length === 4 || minutesInput.value.length === 4 || hoursInput.value.length === 4) {
@@ -100,7 +197,12 @@ function Webpage() {
                 for (const input of inputs) {
 
                     input.style.cssText = "font-size: 20px;"
-                    input.setAttribute("class", "fourValueInput")
+
+                    if (darkMode === false) {
+                        input.classList.add("lightModeFourValueInput")
+                    } else {
+                        input.classList.add("darkModeFourValueInput")
+                    }
 
                 }
 
@@ -109,7 +211,12 @@ function Webpage() {
                 for (const input of inputs) {
 
                     input.style.cssText = "font-size: 15px;"
-                    input.setAttribute("class", "fiveOrMoreThanFiveValueInput")
+
+                    if (darkMode === false) {
+                        input.classList.add("darkModeFiveOrMoreThanFiveValueInput")
+                    } else {
+                        input.classList.add("darkModeFiveOrMoreThanFiveValueInput")
+                    }
 
                 }
 
@@ -118,8 +225,12 @@ function Webpage() {
                 for (const input of inputs) {
 
                     input.style.cssText = "font-size: 45px;"
-                    input.removeAttribute("class")
 
+                    if (darkMode === false) {
+                        input.classList.remove("lightModeThreeValueInput")
+                    } else {
+                        input.classList.remove("darkModeThreeValueInput")
+                    }
                 }
 
             }
@@ -225,8 +336,7 @@ function Webpage() {
             for (const input of inputs) {
 
                 input.readOnly = false;
-                input.removeAttribute("class")
-                input.removeAttribute("style")
+
 
             }
 
@@ -235,8 +345,17 @@ function Webpage() {
     };
 
 
+
     return (
         <>
+
+            <div className="themeChangerAndGithubIconsContainer">
+
+                <a className="githubIconContainer" href="https://github.com/favorodera" target="_blank"><img className="githubIcon" src={darkGithub} alt="" /></a>
+
+                <button className="themeChangerIconContainer" onClick={setTheme}><img className="themeChangerIcon" src={sun} alt="" /></button>
+
+            </div>
 
             <div className="timerContainer">
 
@@ -248,21 +367,21 @@ function Webpage() {
                     <div className="hours">
 
                         <label htmlFor="hours">Hours</label>
-                        <input type="number" id="hours" placeholder="00" name="hours" onChange={inputFontSizeHandler} />
+                        <input type="number" id="hours" placeholder="00" name="hours" onChange={inputFontSizeHandler} className="darkModeInput" />
 
                     </div>
 
                     <div className="minutes">
 
                         <label htmlFor="minutes">Minutes</label>
-                        <input type="number" id="minutes" placeholder="00" name="minutes" onChange={inputFontSizeHandler} />
+                        <input type="number" id="minutes" placeholder="00" name="minutes" onChange={inputFontSizeHandler} className="darkModeInput" />
 
                     </div>
 
                     <div className="seconds">
 
                         <label htmlFor="seconds">Seconds</label>
-                        <input type="number" id="seconds" placeholder="00" name="seconds" onChange={inputFontSizeHandler} />
+                        <input type="number" id="seconds" placeholder="00" name="seconds" onChange={inputFontSizeHandler} className="darkModeInput" />
 
                     </div>
 
@@ -279,8 +398,6 @@ function Webpage() {
                 </div>
 
             </div>
-
-            <a href="https://github.com/favorodera" style={{ color: "white", fontSize: "25px" }}>Codes By favorodera</a>
 
         </>
     )
